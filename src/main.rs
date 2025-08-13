6f9d4176b06733bbd027dc082de5b24bfed71bd9
Rithvik Reddygari <reddygari.rithvik@gmail.com>
2025-08-13 02:37:44 -0400

    finished lexer basic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use regex::Regex;
-use regex::RegexSet;
 fn main() {
-    lex(&String::from("int main() {
+    let res = lex(&String::from("int main() {
     return 2;
 }"));
+    println!("{:?}", res);
 }
 
 fn lex(input: &String) -> Vec<String> {
 
-        // Regex for open bracket
+    // Regex for open bracket
     let open_bracket = Regex::new(r"\{").unwrap();
 
     // Regex for closed bracket
@@ -38,68 +38,97 @@ fn lex(input: &String) -> Vec<String> {
     let mut tokens = Vec::new();
     let mut c = 0;
     while c < input.len() {
-        if let Some(match_result) = open_bracket.find_at(input, c.0) {
-            if match_result.start() == c.0 {
+        // Check if we have whitespace
+        if input.chars().nth(c).unwrap().is_whitespace() {
+            c += 1;
+            continue;
+        }
+
+        let mut best_match: Option<&str> = None;
+        let mut best_length = 0;
+
+
+        if let Some(match_result) = open_bracket.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
                 // Match at current val, push
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
         }
 
-        if let Some(match_result) = closed_bracket.find_at(input, c.0) {
-            if match_result.start() == c.0 {
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+        if let Some(match_result) = closed_bracket.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
+                // Match at current val, push
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
         }
 
-        if let Some(match_result) = open_paranthesis.find_at(input, c.0) {
-            if match_result.start() == c.0 {
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+        if let Some(match_result) = open_paranthesis.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
+                // Match at current val, push
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
         }
 
-        if let Some(match_result) = closed_paranthesis.find_at(input, c.0) {
-            if match_result.start() == c.0 {
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+        if let Some(match_result) = closed_paranthesis.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
+                // Match at current val, push
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
         }
 
-        if let Some(match_result) = semicolon.find_at(input, c.0) {
-            if match_result.start() == c.0 {
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+        if let Some(match_result) = semicolon.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
+                // Match at current val, push
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
         }
 
-        if let Some(match_result) = int_keyword.find_at(input, c.0) {
-            if match_result.start() == c.0 {
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+        if let Some(match_result) = int_keyword.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
+                // Match at current val, push
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
         }
 
-        if let Some(match_result) = return_keyword.find_at(input, c.0) {
-            if match_result.start() == c.0 {
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+        if let Some(match_result) = return_keyword.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
+                // Match at current val, push
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
         }
 
-        if let Some(match_result) = identifier.find_at(input, c.0) {
-            if match_result.start() == c.0 {
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+        if let Some(match_result) = identifier.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
+                // Match at current val, push
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
         }
 
-        if let Some(match_result) = integer_literal.find_at(input, c.0) {
-            if match_result.start() == c.0 {
-                tokens.push(match_result.as_str().to_string());
-                c += match_result.len();
+        if let Some(match_result) = integer_literal.find_at(input, c) {
+            if match_result.start() == c && match_result.len() > best_length {
+                // Match at current val, push
+                best_match = Some(match_result.as_str());
+                best_length = match_result.len();
             }
+        }
+
+        // Push final value, check if invalid
+        if let Some(token) = best_match {
+            tokens.push(token.to_string());
+            c += best_length;
+        } else {
+            // invalid char
+            // we still want to parse it
+            tokens.push(input.chars().nth(c).unwrap().to_string());
+            c += 1;
         }
     }
 
